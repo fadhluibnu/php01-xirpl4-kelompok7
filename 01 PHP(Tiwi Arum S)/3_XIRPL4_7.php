129058122c2ca8b151a41b75faab1bef32992ada
tiwisetiyaningsih <tiwisetiyaningsih@gmail.com>
2021-10-10 12:38:20 +0700

    01 PHP(Tiwi Arum S)

---
+++ 01 PHP(Tiwi Arum S)/3_XIRPL4_7.php
@@ -0,0 +1,17 @@
+
+<?php
+         $nama = ["Satria", "Arsyi", "Dhika", "Evan","Fabian","Irfan"];
+
+         print_r ($nama);
+
+         
+         $data = array(
+            array('nama'=> 'Satria', 'kelas'=> 'Rpl 1'),
+            array('nama'=> 'Arsyi', 'kelas'=> 'Rpl 2'),
+            array('nama'=> 'Dhika', 'kelas'=> 'Rpl 3'),
+            array('nama'=> 'Evan', 'kelas'=> 'Rpl 4'),
+            array('nama'=> 'Fabian', 'kelas'=> 'Rpl 5'),
+            array('nama'=> 'Irfan', 'kelas'=> 'Rpl 6')
+        );
+        print_r($data)
+?>     
---
+++ 01 PHP(Tiwi Arum S)/form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<title>Form.php</title>
+</head>
+<body>  
+<h1>Input Data</h1>
+<form method="GET" action="prosesform.php">
+    <table>
+    <tr>
+        <td>Nama</td> 
+        <td>: <input type="text" name="nama"></td>
+    </tr>
+    <tr>
+        <td>E-Mail</td> 
+        <td>: <input type="text" name="email"></td>
+    </tr>
+    <tr>
+    <td><input type="submit" value="Proses Data" name="Proses Data"></td>
+    </tr>
+    </tabel>
+</form>
+
+</body>
+</html>
