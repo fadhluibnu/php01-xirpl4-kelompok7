d9305feed6993da20165ad3d1a1d3dfa2917ba28
MAgi-15 <3103120140@student.smktelkom-pwt.sch.id>
2021-10-10 12:38:33 +0700

    Agi PHP

---
+++ 01 PHP(Agi)/3_XI RPL4_7.php
@@ -0,0 +1,32 @@
+<?php
+$nama = ["Satria", "Arsyi", "Dhika", "Evan", "Febian", "Irfan"];
+print_r($nama);
+
+$mahasiswa  = [
+    [
+        "Nama" => "Satria",
+        "Kelas" => "XI RPL 1",
+    ],
+    [
+        "Nama" => "Arsyi",
+        "Kelas" => "XI RPL 2",
+    ],
+    [
+        "Nama" => "Dhika",
+        "Kelas" => "XI RPL 3",
+    ],
+    [
+        "Nama" => "Evan",
+        "Kelas" => "XI RPL 4",
+    ],
+    [
+        "Nama" => "Febian",
+        "Kelas" => "XI RPL 5",
+    ],
+    [
+        "Nama" => "Irfan",
+        "Kelas" => "XI RPL 6",
+    ],
+];
+print_r($mahasiswa);
+?>
---
+++ 01 PHP(Agi)/form.php
@@ -0,0 +1,18 @@
+<fieldset id="ini">
+<label for="ini">Input Data</label>
+<form method="GET" action="prosesform.php">
+    <table>
+    <tr>
+        <td>Nama </td>
+        <td>: <input type="text" name="nama"></td>
+    </tr>
+    <tr>
+        <td>E-Mail</td> 
+        <td>: <input type="text" name="email"></td>
+    </tr>
+    <tr>
+    <td><input type="submit" values="Proses" name="submit"></post></td>
+    </tr>
+    </tabel>
+</form>
+</fieldset>
